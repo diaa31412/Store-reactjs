8eda1a97f6cec9f74db5a8cb678636be57d8da85
diaa31412 <dhariri31412@gmail.com>
2019-01-24 19:02:53 +0200

    create page not found

--- src/components/default.js
+++ src/components/default.js
@@ -1,16 +1,21 @@
 import React,{Component} from 'react';
 import styled from 'styled-components';
 
-const Heading = styled.h3`
-    background: red;
-`;
+
 
 
 class Default extends Component {
    render(){
      return(
-       <div>
-         <Heading>Page Not Found</Heading>
+       <div className="container">
+         <div className="row">
+           <div className="col-10 mx-auto text-center text-title pt-5">
+             <h1 className="display-3">404</h1>
+             <h1>error</h1>
+             <h2>page not found</h2>
+             <h3>The requested URL:<span className="text-danger">{this.props.location.pathname}</span> was not found</h3>
+           </div>
+         </div>
        </div>
      );
    }
